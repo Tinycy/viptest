a8adf65c4236adc40e4210568b2cab876b5f1f3d
Tinycy <839435033@qq.com>
2016-08-15 12:06:51 +0800

    mster提交了

--- 唯品会/script/MoreTabView.js
+++ 唯品会/script/MoreTabView.js
@@ -1,29 +1,31 @@
-
-
 function MoreTabView(){
-
-TabView.call(this)
-
+	
+	TabView.call(this)
 }
+
 MoreTabView.prototype = Object.create(TabView.prototype)
 
 MoreTabView.prototype.constructor = MoreTabView
 
 MoreTabView.prototype.load = function(){
-
+	
 	TabView.prototype.load.call(this)
 	
-	var moreRight = document.querySelector('.more-show').getBoundingClientRect().right	
-
-var left = moreRight - $('.more-model').innerWidth() +'px'
-
+	//获取更多按钮的右边和窗口的距离
+	var moreRight = document.querySelector('.more-show').getBoundingClientRect().right
+	
+	var left = moreRight -  $('.more-modal').innerWidth() + 'px'
+	
 	$('.more-modal').show().css({
-	
-	left:left
-	
+		
+		left:left
 	})
 }
+
 MoreTabView.prototype.unload = function(){
-$('.more-modal').hide()
+	
+	$('.more-modal').hide()
 }
+
+
 
